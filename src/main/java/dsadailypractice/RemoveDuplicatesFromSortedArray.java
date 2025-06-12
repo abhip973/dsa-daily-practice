9c6acff4a042d93cd74f7bd30d70eea71cc6372c
abhip973 <punj.abhishek1@gmail.com>
2025-06-12 11:56:56 +0530

    Remove Duplicates from Sorted Array

---
+++ src/main/java/dsadailypractice/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,22 @@
+package dsadailypractice;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    //Leetcode link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(arr));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[x]) {
+                x++;
+                nums[x] = nums[i];
+            }
+        }
+        return ++x;
+    }
+}
