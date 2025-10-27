6c283edcd9f9648e618ba047ef23dca1c8987299
abhip973 <punj.abhishek1@gmail.com>
2025-10-27 20:20:46 +0530

    Inorder and postorder traversal

---
+++ src/main/java/dsadailypractice/BinaryTree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,38 @@
+package dsadailypractice.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        traversal(root, list);
+        return list;
+    }
+
+    public static void traversal(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        traversal(root.left, list);
+        list.add(root.val);
+        traversal(root.right, list);
+    }
+}
