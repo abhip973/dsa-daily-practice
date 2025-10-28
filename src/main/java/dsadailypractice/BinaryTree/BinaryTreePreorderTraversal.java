bb203fc808e80a1c74e9bd96828c21c7f98ea74b
abhip973 <punj.abhishek1@gmail.com>
2025-10-28 12:56:32 +0530

    Preorder traversal using iterative approach

--- src/main/java/dsadailypractice/BinaryTree/BinaryTreePreorderTraversal.java
+++ src/main/java/dsadailypractice/BinaryTree/BinaryTreePreorderTraversal.java
@@ -2,6 +2,7 @@ package dsadailypractice.BinaryTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePreorderTraversal {
     public class TreeNode {
@@ -34,5 +35,19 @@ public class BinaryTreePreorderTraversal {
         list.add(root.val);
         traversal(root.left, list);
         traversal(root.right, list);
+    }
+
+    public List<Integer> preorderTraversalIterativeApproach(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            list.add(curr.val);
+            if (curr.right != null) stack.push(curr.right);
+            if (curr.left != null) stack.push(curr.left);
+        }
+        return list;
     }
 }
