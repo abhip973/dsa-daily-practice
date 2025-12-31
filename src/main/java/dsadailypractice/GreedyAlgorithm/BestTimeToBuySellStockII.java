1e2968ec4b95bd1951432e35c24d88d858b70fe9
abhip973 <punj.abhishek1@gmail.com>
2025-12-31 13:20:06 +0530

    Best Time to Buy and Sell Stocks II

---
+++ src/main/java/dsadailypractice/GreedyAlgorithm/BestTimeToBuySellStockII.java
@@ -0,0 +1,13 @@
+package dsadailypractice.GreedyAlgorithm;
+
+public class BestTimeToBuySellStockII {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - prices[i - 1];
+            if (profit > 0)
+                ans += profit;
+        }
+        return ans;
+    }
+}
