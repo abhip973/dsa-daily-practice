eb100db9da4e7651ea240b1749d4a28afd22c4cd
abhip973 <punj.abhishek1@gmail.com>
2026-02-09 10:03:03 +0530

    Find if Path Exists in Graph

---
+++ src/main/java/dsadailypractice/Graphs/FindIfPathExistsInGraph.java
@@ -0,0 +1,49 @@
+package dsadailypractice.Graphs;
+
+import java.util.*;
+
+public class FindIfPathExistsInGraph {
+    public boolean validPath(int n, int[][] edges, int src, int dest) {
+        if (src == dest) return true;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            int zero = edges[i][0];
+            int first = edges[i][1];
+            if (map.containsKey(zero)) {
+                List<Integer> li = map.get(zero);
+                li.add(first);
+                map.put(zero, li);
+            } else {
+                List<Integer> li = new ArrayList<>();
+                li.add(first);
+                map.put(zero, li);
+            }
+            if (map.containsKey(first)) {
+                List<Integer> li = map.get(first);
+                li.add(zero);
+                map.put(first, li);
+            } else {
+                List<Integer> li = new ArrayList<>();
+                li.add(zero);
+                map.put(first, li);
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+        Set<Integer> visited = new HashSet<>();
+        visited.add(src);
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if (curr == dest) return true;
+            List<Integer> nei = map.get(curr);
+            if (nei == null) continue;
+            for (int p : nei) {
+                if (!visited.contains(p)) {
+                    q.offer(p);
+                    visited.add(p);
+                }
+            }
+        }
+        return false;
+    }
+}
